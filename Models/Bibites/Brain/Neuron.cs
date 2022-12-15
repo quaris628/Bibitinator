dc35b0f949cbcdbef12bca30069aae2ed8a86eef
Quaris <quaris314@gmail.com>
2022-12-14 23:35:25 -0600

    Created Neuron and Synapse classes

---
+++ Models/Bibites/Brain/Neuron.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibitinator.Models.Bibites.Brain
+{
+    public class Neuron
+    {
+        public enum Types
+        {
+            Input = 0,
+            Sigmoid = 1,
+            Linear = 2,
+            TanH = 3,
+            Sine = 4,
+            ReLu = 5,
+            Gaussian = 6,
+            Latch = 7,
+            Differential = 8,
+            Abs = 9,
+        }
+
+        private static int idCount = 0;
+
+        public int Index { get; }
+        public Types Type { get; set; }
+        public string Description { get; set; } // more like a name
+
+        public Neuron(Types type, string? description)
+        {
+            if (description == null) { description = "Hidden" + Index; }
+            else if (!description.All(char.IsLetterOrDigit)) { throw new InvalidDescException(); }
+            // TODO check if a neuron of that name already exists?
+
+            Index = idCount++;
+            Type = type;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            // TODO test if Type.ToString() returns what I think it does
+            return Description + Type.ToString();
+        }
+    }
+    public class InvalidDescException : Exception { }
+}
---
+++ Models/Bibites/Brain/Synapse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibitinator.Models.Bibites.Brain
+{
+    public class Synapse
+    {
+        private static int idCount = 0;
+        public int Index { get; set; }
+        public Neuron From { get; set; }
+        public Neuron To { get; set; }
+        public float Strength { get; set; }
+
+        public Synapse(Neuron from, Neuron to, float strength)
+        {
+            if (from == null || to == null) { throw new ArgumentNullException(); }
+            if (from.Equals(to)) { throw new SameNeuronException(); }
+            strength = Math.Max(-10f, Math.Min(strength, 10));
+
+            Index = idCount++;
+            From = from;
+            To = to;
+            Strength = Math.Max(-10f, Math.Min(strength, 10));
+        }
+
+        public override string ToString()
+        {
+            return From.ToString() + " " + To.ToString() + " " + Math.Round(Strength,2);
+        }
+    }
+
+    public class SameNeuronException : Exception { }
+}
